f08bb01fdd2ccbdd914f0058bd6c2bd0457c216d
Pedro Alves <palves1945@hotmail.com>
2023-05-31 22:58:06 -0300

    Update movies_api.py __init__.py resources.py 2023-05-31 22:58

--- backend/movies/__init__.py
+++ backend/movies/__init__.py
@@ -15,6 +15,7 @@ movies_api.add_url_rule(
 movies_api.add_url_rule("/movies", view_func=movies, methods=["POST"])
 
 movies_api.add_url_rule(
-    "/movies/<int:movie_id>", view_func=movies, methods=["GET", "PUT", "DELETE"]
+    "/movies/<int:movie_id>", view_func=movies, 
+    methods=["GET", "PUT", "DELETE"]
 )
 
